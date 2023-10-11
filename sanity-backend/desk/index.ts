6c12e2d8d8952b03d853078a55d209225b99620c
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-12 00:30:39 +0100

    structure builder for desk

---
+++ sanity-backend/desk/index.ts
@@ -0,0 +1,54 @@
+/**
+ * Desk structure overrides
+ */
+import {ListItemBuilder, StructureResolver} from 'sanity/desk'
+import colorThemes from './colorThemeStructure'
+import home from './homeStructure'
+import pages from './pageStructure'
+import category from './categoryStructure'
+import settings from './settingStructure'
+
+/**
+ * Desk structure overrides
+ *
+ * Sanity Studio automatically lists document types out of the box.
+ * With this custom desk structure we achieve things like showing the `home`
+ * and `settings` document types as singletons, and grouping product details
+ * and variants for easy editorial access.
+ *
+ * You can customize this even further as your schemas progress.
+ * To learn more about structure builder, visit our docs:
+ * https://www.sanity.io/docs/overview-structure-builder
+ */
+
+// If you add document types to desk structure manually, you can add them to this function to prevent duplicates in the root pane
+const hiddenDocTypes = (listItem: ListItemBuilder) => {
+  const id = listItem.getId()
+
+  if (!id) {
+    return false
+  }
+
+  return ![
+    'collection',
+    'colorTheme',
+    'home',
+    'media.tag',
+    'page',
+    'product',
+    'category',
+    'productVariant',
+    'settings',
+  ].includes(id)
+}
+
+export const structure: StructureResolver = (S, context) =>
+  S.list()
+    .title('Content')
+    .items([
+      pages(S, context),
+      S.divider(),
+      category(S, context),
+      S.divider(),
+      ...S.documentTypeListItems().filter(hiddenDocTypes),
+    ])
