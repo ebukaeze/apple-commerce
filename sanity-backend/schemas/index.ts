caf7cec45f38990bbf71f2fc53386f9b906fe243
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-12 00:32:14 +0100

    index and pages for sanity documents

---
+++ sanity-backend/schemas/index.ts
@@ -0,0 +1,10 @@
+
+import page from './documents/page'
+
+import categories from './documents/categories'
+import productVariant from './documents/productVariant'
+import productDoc from './documents/productDoc'
+
+const documents = [productDoc, page, categories]
+
+export const schemaTypes = [...documents]
