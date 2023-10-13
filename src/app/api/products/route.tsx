80490015f6950feccb31642656558a9ff3c7f373
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-13 22:25:53 +0100

    added groq query to fetch products data

---
+++ src/app/api/products/route.tsx
@@ -0,0 +1,19 @@
+import { sanityClient } from "../../../../sanity";
+import { groq } from "next-sanity";
+
+// GROQ query
+const query = groq`*[_type == "productDoc"]{
+  _id,
+    ...
+} | order(_createdAt desc)`;
+
+
+type Data ={
+    products: Product[],
+}
+
+export async function GET(request: Request, response: Response) {
+   const products = await sanityClient.fetch(query);
+
+   return new Response(JSON.stringify({products}));
+}
