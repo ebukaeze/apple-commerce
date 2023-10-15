7539eb71c34cd816a7bf11550e4a65c12f99ddfb
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-15 14:02:57 +0100

    added react toast for notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import { Providers } from './redux/provider'
+import toast, {Toaster} from 'react-hot-toast'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Apple redesign',
   description: 'Get the latest apple technologies',
 }
+
 
 export default function RootLayout({
   children,
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Toaster/>
+        {children}
+        </Providers>   
+        </body>
     </html>
   )
 }
