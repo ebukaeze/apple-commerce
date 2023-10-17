cd07f23e00ea6eb55c69c25eb0b1b3fea361af73
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-17 18:55:00 +0100

    explore page for products. helper functions

--- src/app/components/Explore.tsx
+++ src/app/components/Explore.tsx
@@ -1,58 +1,68 @@
-import React from 'react';
-import Image from 'next/image';
-import { urlFor } from '../../../sanity';
+import React from "react";
+import Image from "next/image";
+import { urlFor } from "../../../sanity";
 
-
-interface Props{
-    products: Product;
+interface Props {
+  products: Product;
 }
 
-const Explore = ({products}: Props) => {
-
+const Explore = ({ products }: Props) => {
+  const truncate = (str: string, n: number) => {
+    return str.length <= n ? str : str.substring(0, n) + "...";
+  };
   return (
     <div>
-        <div className='flex flex-col'>
-                <div className='w-full flex py-4 items-start justify-center space-y-6 mt-12'>
-                <div className='flex-none relative w-48'>
-                    <Image src={urlFor(products.image[0]).url()} alt={products._id}
-                    className='flex inset-0 h-[280px] w-[230px] object-contain' width={240} height={320}
-                    loading='lazy'/>
-                    </div>
-                    <form className="flex-auto p-6 bg-white rounded-lg drop-shadow-md">
-    <div className="flex flex-wrap">
-      <h1 className="flex-auto text-lg font-semibold text-slate-900">
-        {products.title}
-      </h1>
-      <div className="text-lg font-semibold text-slate-500">
-        ${products.price}
+      <div className="flex flex-col">
+        <div
+          className="w-full flex py-4 flex-col lg:flex-row items-start 
+        justify-center lg:space-y-6 mt-12 bg-white px-4 rounded-lg drop-shadow-md"
+        >
+          <div className="flex-none relative w-44">
+            <Image
+              src={urlFor(products.image[0]).url()}
+              alt={products._id}
+              className="flex inset-0 h-[280px] w-[230px] object-contain"
+              width={230}
+              height={280}
+              loading="lazy"
+            />
+          </div>
+          <form className="flex flex-col lg:w-[70%] flex-auto p-6 border-l">
+            <div className="flex flex-wrap">
+              <h1 className="flex-auto text-lg font-semibold text-slate-900">
+                {products.title}
+              </h1>
+              <div className="text-lg font-semibold text-slate-500">
+                ${products.price}
+              </div>
+              <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2">
+                In stock
+              </div>
+            </div>
+            <p className="text-sm text-slate-700 py-4">
+              {truncate(products.description, 70)}
+            </p>
+            <div className="flex w-full flex-wrap flex-row py-4 place-items-center justify-center space-x-4">
+              <div className="w-full flex space-y-4 lg:space-y-0 lg:space-x-4 lg:flex-row flex-col items-center justify-center">
+                <button
+                  className="flex w-[100%] h-10 px-3 lg:px-4 items-center justify-center font-semibold rounded-md bg-black text-white"
+                  type="submit"
+                >
+                  Buy now
+                </button>
+                <button
+                  className="flex w-[100%] items-center justify-center h-10 px-3 lg:px-4 font-semibold rounded-md border border-slate-200 text-slate-900"
+                  type="button"
+                >
+                  Add to bag
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2">
-        In stock
-      </div>
     </div>
-    <div className="flex space-x-4 mb-6 text-sm font-medium py-4">
-      <div className="flex-auto flex space-x-4">
-        <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
-          Buy now
-        </button>
-        <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
-          Add to bag
-        </button>
-      </div>
-      <button className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
-        <svg width="20" height="20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
-        </svg>
-      </button>
-    </div>
-    <p className="text-sm text-slate-700 py-4">
-      {products.description}
-    </p>
-    </form>
-    </div>
-       </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Explore
+export default Explore;
--- src/app/components/ExploreTab.tsx
+++ src/app/components/ExploreTab.tsx
@@ -1,58 +1,67 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Tab } from '@headlessui/react';
-import Explore from './Explore';
+import React from "react";
+import { Tab } from "@headlessui/react";
+import Explore from "./Explore";
 
-interface Props{
-    categories: Category[],
-    products: Product[],
-   }
-
-
-const ExploreTab = ({categories, products}: Props) => {
-
-  //function to show product based on category
-  const showProducts = (category: number) => {
-    return products.filter((product) => product.category._ref === categories[category]._id)
-    .map((product) => <Explore products={product} key={product._id}/>)
-   
-  }
-  console.log(showProducts(0));
-  return (
-    <div className='w-full flex flex-col mt-6 '>
-  <Tab.Group>
-    <Tab.List className="flex items-center justify-center bg-slate-50 drop-shadow-md 
-    transition-shadow h-[80px]">
-       {categories.map((category) => (
-        <Tab 
-         key={category._id}
-         id={category._id}
-         className={({selected}) => 
-         `whitespace-nowrap rounded-t-lg pt-3 px-5 text-sm font-medium
-         outline-none md:pt-4 md:px-6 md:text-base pb-0 ${
-          selected ?
-          "borderGradient bg-[#8b8b8b] text-black"
-          : "border-b-2 border-[#e7e7e7] text-[#808182]"
-         }`
-         }
-         >
-          {category.title}
-        </Tab>
-      ))} 
-    </Tab.List>
-      <Tab.Panels className='w-[80%] mx-auto max-w-6xl pt-10 pb-24 sm:px-4 flex
-      lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16'>
-      <Tab.Panel className='tabPanel'>{showProducts(0)}</Tab.Panel>
-      <Tab.Panel className='tabPanel'>{showProducts(1)}</Tab.Panel>
-      <Tab.Panel className='tabPanel'>{showProducts(2)}</Tab.Panel>
-      <Tab.Panel className='tabPanel'>{showProducts(3)}</Tab.Panel>  
-      
-    </Tab.Panels>
-
-  </Tab.Group>
-    </div>
-  )
+interface Props {
+  categories: Category[];
+  products: Product[];
 }
 
-export default ExploreTab
+const ExploreTab = ({ categories, products }: Props) => {
+  //function to show product based on category
+  const showProducts = (category: number) => {
+    return products
+      .filter((product) => product.category._ref === categories[category]._id)
+      .map((product) => <Explore products={product} key={product._id} />);
+  };
+  console.log(showProducts(0));
+  return (
+    <div className="w-full flex flex-col mt-6 ">
+      <Tab.Group>
+        <Tab.List
+          className="flex items-center justify-center bg-slate-50 
+     h-[80px] transition-all"
+        >
+          {categories.map((category) => (
+            <Tab
+              key={category._id}
+              id={category._id}
+              className={({ selected }) =>
+                `whitespace-nowrap rounded-t-lg pt-3 px-5 text-sm font-medium
+         outline-none md:pt-4 md:px-6 md:text-base pb-0 transition ease-in-out duration-300
+         ${
+           selected
+             ? "borderGradient bg-[#8b8b8b] text-black transition ease-in-out hover:translate-y-1 duration-300"
+             : "border-b-2 border-[#e7e7e7] text-[#808182]"
+         }`
+              }
+            >
+              {category.title}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels
+          className="w-[80%] mx-auto max-w-6xl pt-10 pb-24 sm:px-4 flex
+      lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16"
+        >
+          <Tab.Panel className="tabPanel lg:grid md:grid-cols-2 sm:grid-cols-1 xl:grid-cols-2 gap-8">
+            {showProducts(0)}
+          </Tab.Panel>
+          <Tab.Panel className="tabPanel lg:grid md:grid-cols-2 sm:grid-cols-1 xl:grid-cols-2 gap-8">
+            {showProducts(1)}
+          </Tab.Panel>
+          <Tab.Panel className="tabPanel lg:grid md:grid-cols-2 sm:grid-cols-1 xl:grid-cols-2 gap-8">
+            {showProducts(2)}
+          </Tab.Panel>
+          <Tab.Panel className="tabPanel lg:grid md:grid-cols-2 sm:grid-cols-1 xl:grid-cols-2 gap-8">
+            {showProducts(3)}
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
+};
+
+export default ExploreTab;
