f17fa4810fc9b57775f2af8464abb948a05d0a6a
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-27 17:32:12 +0100

    promise function to get the stripe checkout ses...

---
+++ src/app/libs/api-helpers.tsx
@@ -0,0 +1,23 @@
+export async function FetchPostJSON(url: string, data?: {}) {
+  try {
+    const response = await fetch(url, {
+      method: "POST", // Post, Get, Update, Delete etc
+      mode: "cors", //no cors, *cors, same origin
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "content-type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data || {}), //body data must match content type
+    });
+
+    return await response.json();
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+    throw error;
+  }
+}
