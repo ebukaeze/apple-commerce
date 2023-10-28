5482d46f3cdb09bae23888872a49dc5bcf89e327
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-28 20:08:34 +0100

    mobile responsiveness for the banner section

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -6,12 +6,12 @@ import Link from "next/link";
 const Banner = () => {
   return (
     <section
-      className="w-full flex bg-slate-300  md:max-w-[1229px] md:mx-auto items-center justify-center flex-col h-[90vh]
-      space-y-14 gap-y-12 sticky top-0 md:flex-row md:justify-evenly text-center sm:text-center mt-12 md:mt-2"
+      className="w-full grid grid-cols-1 items-center lg:grid-cols-9 bg-[rgba(250,250,250,0.5)]  md:max-w-[1229px] md:mx-auto h-[90vh]
+      space-y-1 gap-y-1 sticky top-0 justify-items-stretch it sm:text-start mt-14 md:mt-2"
     >
-      <div className="w-full flex flex-col items-center justify-center md:justify-start space-y-9 gap-y-8">
-        <div className="w-[100%] flex flex-col px-8 place-items-start before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-pink-200 after:via-sky-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-          <h1 className="text-3xl md:text-6xl text-black font-[900] sm:text-[24px]">
+      <div className="w-full grid col-span-5 order-1 col-start-1 lg:justify-self-start space-y-6 gap-y-4">
+        <div className="w-[100%] flex flex-col px-8 lg:text-start text-center before:absolute before:h-[300px] lg:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] lg:after:translate-x-1/3 after:bg-gradient-conic after:from-pink-200 after:via-sky-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+          <h1 className="text-5xl tracking-wide justify-self-start md:text-6xl text-black font-[900] sm:text-[24px] ">
             {" "}
             Empower your Vision{" "}
           </h1>
@@ -19,7 +19,7 @@ const Banner = () => {
             with the New iphone 15 pro
           </span>
         </div>
-        <div className="flex gap-x-4 w-[100%] px-8">
+        <div className="flex items-center lg:justify-start justify-center gap-x-4 w-[100%] px-8">
           <Link href={"/explore"}>
             <Button
               title="Be Pro"
@@ -45,12 +45,12 @@ const Banner = () => {
         </div>
       </div>
       <div
-        className="flex relative h-[450px] w-[450px] sm:w-[450px] transition-all duration-100 
-          md:line md:h-[550px] md:w-[550px] md:block mt-2 md:mt-0
-          place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-2/3 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] md:after:w-[240px] md:after:translate-x-1/6 after:w-[240px] after:translate-x-2/8 after:bg-gradient-conic after:from-pink-800 after:via-sky-300 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
+        className="lg:col-span-4 order-2 relative h-[450px] w-[100%] lg:w-[550px] sm:w-[100%] transition-all duration-100 
+          md:line md:h-[550px] md:w-[850px] md:block mt-2 md:mt-0
+          place-items-center before:absolute before:h-[300px] lg:before:w-[400px] before:w-[50px] lg:before:-translate-x-1/3 before:translate-x-1/3 text-center before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] md:after:w-[400px] md:after:translate-x-1/4 after:w-[200px] after:translate-x-1/6 after:bg-gradient-conic after:from-pink-800 after:via-sky-300 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
       >
         <Image
-          src="/iphone3.png"
+          src="/appleflat.png"
           alt="iphone 14 pro max"
           className="object-contain relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           fill
