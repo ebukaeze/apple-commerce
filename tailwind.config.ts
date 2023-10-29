494a339959b9de9cc27e429fab5a46538e06dfc5
Ebuka Eze <wizzyblaq94@gmail.com>
2023-10-29 15:19:23 +0100

    pacxkages and custom tailwindcss styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-pattern": "url('../../public/macbook.jpg')",
       },
       fontFamily: {
-        body: ['Poppins', 'sans-serif'],
+        body: ["Poppins", "sans-serif"],
       },
       gridTemplateRows: {
         // Simple 8 row grid
-        '2': 'repeat(8, minmax(256px, 1fr))',
-      }
+        "2": "repeat(8, minmax(256px, 1fr))",
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
