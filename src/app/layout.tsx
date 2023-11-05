9d011ff6b50d6cac4d131bfeabaa23f65164cb30
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-11-06 00:54:37 +0100

    wrapped the auth provided around our entire app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,37 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Providers } from './redux/provider'
-import toast, {Toaster} from 'react-hot-toast'
-const inter = Inter({ subsets: ['latin'] })
-
+import "./globals.css";
+import type { Metadata } from "next";
+import type { AppProps } from "next/app";
+import { Inter, Poppins, Lora } from "next/font/google";
+import { Providers } from "./redux/provider";
+import AuthProvider from "./context/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
+const inter = Inter({ subsets: ["latin"] });
+const lora = Lora({ subsets: ["latin"] });
+const poppinsFont = Poppins({
+  weight: "500",
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 export const metadata: Metadata = {
-  title: 'Apple redesign',
-  description: 'Get the latest apple technologies',
-}
-
+  title: "Apple redesign",
+  description: "Get the latest apple technologies",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          <Toaster/>
-        {children}
-        </Providers>   
-        </body>
+      <body className={`${poppinsFont.variable}`}>
+        <AuthProvider>
+          <Providers>
+            <Toaster />
+            {children}
+          </Providers>
+        </AuthProvider>
+      </body>
     </html>
-  )
+  );
 }
