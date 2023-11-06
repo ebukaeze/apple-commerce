a75f28ad2da5c35c66727dd26a5f0ed5561828f3
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-11-06 01:43:23 +0100

    Update store.tsx

--- src/app/redux/store.tsx
+++ src/app/redux/store.tsx
@@ -2,12 +2,10 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './cartSlice';
-import counterSlice from './counterSlice';
 
 export const store = configureStore({
     reducer: {
         cart: cartSlice,
-        counter: counterSlice
     },
 });
 
