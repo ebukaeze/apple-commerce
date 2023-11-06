bb16ff377c61833a87640ffbdd2fac3d555a7853
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-11-06 15:29:07 +0100

    added type interface

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,26 +1,17 @@
-import React from 'react'
-import Products from '../components/Products'
-import { fetchProducts } from '../libs/fetchProducts'
-import { fetchCategories } from '../libs/fetchCategories'
-import Image from 'next/image'
-import { urlFor } from '../../../sanity'
-import ExploreTab from '../components/ExploreTab'
+import React from "react";
+import Products from "../components/Products";
+import { fetchProducts } from "../libs/fetchProducts";
+import { fetchCategories } from "../libs/fetchCategories";
+import Image from "next/image";
+import { urlFor } from "../../../sanity";
+import ExploreTab from "../components/ExploreTab";
 
-interface Props{
-products: Product[],
-categories: Category[],
-}
-
-const page = async ({products, categories}: Props) => {
-    const productItems = await fetchProducts();
-    const categoriesItems = await fetchCategories();
+export default async function ExplorePage() {
+  const productItems: Product[] = await fetchProducts();
+  const categoriesItems: Category[] = await fetchCategories();
   return (
-    <div className='flex w-full py-6 mt-4 space-y-6'>
-        
-        
-        <ExploreTab categories={categoriesItems} products={productItems}/>
-        </div>
-  )
+    <div className="flex w-full py-6 mt-4 space-y-6">
+      <ExploreTab categories={categoriesItems} products={productItems} />
+    </div>
+  );
 }
-
-export default page
