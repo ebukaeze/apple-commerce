06899dcc2f8038e26a5c0b5b539604c7d8d11828
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-11-06 15:02:56 +0100

    category strucure definition

--- sanity-backend/desk/categoryStructure.ts
+++ sanity-backend/desk/categoryStructure.ts
@@ -2,5 +2,5 @@ import {ListItemBuilder} from 'sanity/desk'
 import defineStructure from '../utils/defineStructure'
 
 export default defineStructure<ListItemBuilder>((S) =>
-  S.listItem().title('Category').schemaType('category').child(S.documentTypeList('category'))
+  S.listItem().title('Category').schemaType('category').child(S.documentTypeList('category')),
 )
