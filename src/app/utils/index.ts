4e75d4cc71963c528c5e033621b7328a1e4d8b85
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-04-06 11:17:17 +0100

    refactor: change the video directory

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -2,15 +2,6 @@ import hero from "/public/images/hero.jpeg";
 
 export const heroImg = hero;
 
-import hmv from "/videos/hero.mp4";
-import smallmv from "/videos/smallHero.mp4";
-import highlightFirstmv from "/videos/highlight-first.mp4";
-import highlightSectmv from "/videos/hightlight-third.mp4";
-import highlightThirdmv from "/videos/hightlight-sec.mp4";
-import highlightFourthmv from "/videos/hightlight-fourth.mp4";
-import exploremv from "/videos/explore.mp4";
-import framemv from "/videos/frame.mp4";
-
 import apple from "/public/images/apple.svg";
 import search from "/public/images/search.svg";
 import bag from "/public/images/bag.svg";
@@ -28,15 +19,6 @@ import explore1 from "/public/images/explore1.jpg";
 import explore2 from "/public/images/explore2.jpg";
 import chip from "/public/images/chip.jpeg";
 import frame from "/public/images/frame.png";
-
-export const heroVideo = hmv;
-export const smallHeroVideo = smallmv;
-export const highlightFirstVideo = highlightFirstmv;
-export const highlightSecondVideo = highlightSectmv;
-export const highlightThirdVideo = highlightThirdmv;
-export const highlightFourthVideo = highlightFourthmv;
-export const exploreVideo = exploremv;
-export const frameVideo = framemv;
 
 export const appleImg = apple;
 export const searchImg = search;
